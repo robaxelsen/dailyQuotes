5448cf2bf4105892b23fdf45b0e56d8aba9025ad
Robert Axelsen <rob@rob.ee>
2018-01-21 19:35:26 +0100

    Fix syntax error

--- data.go
+++ data.go
@@ -1,5 +1,5 @@
 package dailyQuotes
 
 var Quotes = map[int]string{
-	1: "'They say a person needs just three things to be truly happy in this world: someone to love, something to do, and something to hope for.' - Tom Bodett",
+	1: "They say a person needs just three things to be truly happy in this world: someone to love, something to do, and something to hope for. - Tom Bodett",
 }
