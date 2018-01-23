23748ae9a90d9b7960c29ed59642276c11fec58f
Robert Axelsen <rob@rob.ee>
2018-01-23 21:25:01 +0100

    Add another quote

--- data.go
+++ data.go
@@ -2,4 +2,5 @@ package dailyQuotes
 
 var Quotes = map[int]string{
 	1: "They say a person needs just three things to be truly happy in this world: someone to love, something to do, and something to hope for. - Tom Bodett",
+	2: "Be kind; everyone you meet is fighting a hard battle. - Ian Maclaren",
 }
